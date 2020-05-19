a821f87a7a18e0bd656e2e5beeec06b520e74726
PSK <wiv821253@gmail.com>
2020-05-19 18:04:48 +0900

    remove password

--- spring-boot-oauth2-server/src/main/java/org/psawesome/springbootoauth2server/config/SecurityConfig.java
+++ spring-boot-oauth2-server/src/main/java/org/psawesome/springbootoauth2server/config/SecurityConfig.java
@@ -3,15 +3,10 @@ package org.psawesome.springbootoauth2server.config;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 //import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
@@ -19,12 +14,12 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
+/*
     @Bean
     public PasswordEncoder passwordEncoder() {
 //        DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder();
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
