92caf8f5321d6ee656f36808f097afc4c1625ee0
PSK <wiv821253@gmail.com>
2020-05-21 14:43:39 +0900

    tomcat version

---
+++ spring-boot-oauth2-server-tomcat/src/main/java/org/psawesome/springbootoauth2servertomcat/SpringBootOauth2ServerTomcatApplication.java
@@ -0,0 +1,13 @@
+package org.psawesome.springbootoauth2servertomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2ServerTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2ServerTomcatApplication.class, args);
+    }
+
+}
