aac043263d0f318aac48e8deb7724329ea60cefb
PSK <wiv821253@gmail.com>
2020-05-23 19:35:10 +0900

    **** JWT 전환 ****

--- spring-boot-oauth2-tomcat-jdbc/src/main/java/org/psawesome/springbootoauth2tomcatjdbc/config/OAuth2ServerConfig.java
+++ spring-boot-oauth2-tomcat-jdbc/src/main/java/org/psawesome/springbootoauth2tomcatjdbc/config/OAuth2ServerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.sql.DataSource;
 
@@ -28,9 +28,20 @@ public class OAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
-
+/*
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.tokenStore(new JdbcTokenStore(dataSource));
+    }
+*/
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(this.jwtAccessTokenConverter());
+    }
+
+    JwtAccessTokenConverter jwtAccessTokenConverter() {
+        return new JwtAccessTokenConverter();
     }
 }
