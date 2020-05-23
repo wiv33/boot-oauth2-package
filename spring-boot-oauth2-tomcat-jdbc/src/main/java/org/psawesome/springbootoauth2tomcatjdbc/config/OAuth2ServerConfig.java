60a34cd89870d81924a66e08aa30b56dcb1eca3f
PSK <wiv821253@gmail.com>
2020-05-23 19:27:38 +0900

    complete db

--- spring-boot-oauth2-tomcat-jdbc/src/main/java/org/psawesome/springbootoauth2tomcatjdbc/config/OAuth2ServerConfig.java
+++ spring-boot-oauth2-tomcat-jdbc/src/main/java/org/psawesome/springbootoauth2tomcatjdbc/config/OAuth2ServerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
 
@@ -27,4 +29,8 @@ public class OAuth2ServerConfig extends AuthorizationServerConfigurerAdapter {
         clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
 
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(new JdbcTokenStore(dataSource));
+    }
 }
--- spring-boot-oauth2-tomcat-jdbc/src/main/resources/schema.sql
+++ spring-boot-oauth2-tomcat-jdbc/src/main/resources/schema.sql
@@ -1,16 +1,50 @@
-create table IF NOT EXISTS oauth_client_details
+DROP TABLE IF EXISTS oauth_client_token CASCADE;
+DROP TABLE IF EXISTS oauth_access_token CASCADE;
+DROP TABLE IF EXISTS oauth_refresh_token CASCADE;
+DROP TABLE IF EXISTS oauth_code CASCADE;
+DROP TABLE IF EXISTS oauth_approvals CASCADE;
+
+create table IF NOT EXISTS oauth_client_token
 (
-    client_id               VARCHAR(256) PRIMARY KEY,
-    resource_ids            VARCHAR(256),
-    client_secret           VARCHAR(256),
-    scope                   VARCHAR(256),
-    authorized_grant_types  VARCHAR(256),
-    web_server_redirect_uri VARCHAR(256),
-    authorities             VARCHAR(256),
-    access_token_validity   INTEGER,
-    refresh_token_validity  INTEGER,
-    additional_information  VARCHAR(4096),
-    autoapprove             VARCHAR(256)
+    token_id          VARCHAR(256),
+    token             bytea,
+    authentication_id VARCHAR(256) PRIMARY KEY,
+    user_name         VARCHAR(256),
+    client_id         VARCHAR(256)
+);
+
+create table IF NOT EXISTS oauth_access_token
+(
+    token_id          VARCHAR(256),
+    token             bytea,
+    authentication_id VARCHAR(256) PRIMARY KEY,
+    user_name         VARCHAR(256),
+    client_id         VARCHAR(256),
+    authentication    bytea,
+    refresh_token     VARCHAR(256)
+);
+
+create table IF NOT EXISTS oauth_refresh_token
+(
+    token_id       VARCHAR(256),
+    token          bytea,
+    authentication bytea
+);
+
+create table IF NOT EXISTS oauth_code
+(
+    code           VARCHAR(256),
+    authentication bytea
+);
+
+create table IF NOT EXISTS oauth_approvals
+(
+    userId         VARCHAR(256),
+    clientId       VARCHAR(256),
+    scope          VARCHAR(256),
+    status         VARCHAR(10),
+    expiresAt      TIMESTAMP,
+    lastModifiedAt TIMESTAMP
 );
 
 create table IF NOT EXISTS my_user
@@ -21,4 +55,4 @@ create table IF NOT EXISTS my_user
     provider varchar(100),
     uid      varchar(50)  not null,
     primary key (msrl)
-)
+);
