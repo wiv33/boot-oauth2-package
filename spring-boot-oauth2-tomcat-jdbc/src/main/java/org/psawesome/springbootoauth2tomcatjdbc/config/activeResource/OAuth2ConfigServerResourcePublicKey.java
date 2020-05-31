83b5ef4fe1e710d9ce05b87720a2a86cef1f0440
PSK <wiv821253@gmail.com>
2020-05-31 23:24:03 +0900

    비대칭 키로 전환

--- spring-boot-oauth2-tomcat-jdbc/src/main/java/org/psawesome/springbootoauth2tomcatjdbc/config/activeResource/OAuth2ConfigServerResourcePublicKey.java
+++ spring-boot-oauth2-tomcat-jdbc/src/main/java/org/psawesome/springbootoauth2tomcatjdbc/config/activeResource/OAuth2ConfigServerResourcePublicKey.java
@@ -1,15 +1,21 @@
 package org.psawesome.springbootoauth2tomcatjdbc.config.activeResource;
 
 import lombok.RequiredArgsConstructor;
+import org.psawesome.springbootoauth2tomcatjdbc.service.security.CustomUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import javax.sql.DataSource;
 
 /**
  * package: org.psawesome.springbootoauth2tomcatjdbc.config.activeResource
@@ -22,11 +28,30 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 @RequiredArgsConstructor
 public class OAuth2ConfigServerResourcePublicKey extends AuthorizationServerConfigurerAdapter {
 
+  private final CustomUserDetailService userDetailService;
+  private final PasswordEncoder passwordEncoder;
+  private final DataSource dataSource;
+
   @Override
   public void configure(AuthorizationServerSecurityConfigurer security) {
     security.tokenKeyAccess("permitAll()")
             .checkTokenAccess("isAuthenticated()") //allow check token
             .allowFormAuthenticationForClients();
+  }
+
+
+  /**
+   * 클라이언트 정보 주입 방식을 jdbcdetail로 변경
+   */
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    super.configure(endpoints);
+    endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
   }
 
   /**
