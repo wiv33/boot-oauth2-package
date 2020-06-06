60f46f4160ec142a75260fa7802baa567cf43e3d
PSK <40161102+wiv33@users.noreply.github.com>
2020-06-07 00:22:08 +0900

    compile error

--- spring-boot-oauth2-server/src/main/java/org/psawesome/springbootoauth2server/config/ServiceReactiveUserDetailsService.java
+++ spring-boot-oauth2-server/src/main/java/org/psawesome/springbootoauth2server/config/ServiceReactiveUserDetailsService.java
@@ -1,8 +1,10 @@
 package org.psawesome.springbootoauth2server.config;
 
 import lombok.RequiredArgsConstructor;
+import org.psawesome.springbootoauth2server.user.MyUser;
 import org.psawesome.springbootoauth2server.user.UserService;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsPasswordService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,7 +18,7 @@ import java.util.Collection;
  * DATE: 2020-06-06 토요일 23:22
  */
 @RequiredArgsConstructor
-public class ServiceReactiveUserDetailsService implements ReactiveUserDetailsService {
+public class ServiceReactiveUserDetailsService implements ReactiveUserDetailsService, ReactiveUserDetailsPasswordService {
   private final UserService users;
 
   @Override
@@ -24,6 +26,18 @@ public class ServiceReactiveUserDetailsService implements ReactiveUserDetailsSer
     return this.users.findByEmail(username)
             .map(CustomUser::new);
   }
+
+  @Override
+  public Mono<UserDetails> updatePassword(UserDetails user, String newPassword) {
+    return this.users.findByEmail(user.getUsername())
+            .doOnSuccess(u -> u.)
+            .map(this::create);
+  }
+
+  private Mono<UserDetails> create(MyUser user) {
+
+  }
+
   private class CustomUser extends User {
 
     public CustomUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
